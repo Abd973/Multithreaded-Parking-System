c46939095a8f47c1edff25a37ce5f961d4c13eda
abdulmalik <abdelmaleka011@gmail.com>
2024-11-13 17:54:24 +0200

    Built-in semaphore, the output has some issues

--- src/CarThread.java
+++ src/CarThread.java
@@ -1,17 +1,22 @@
-import java.sql.SQLOutput;
 import java.util.concurrent.Semaphore;
 
 public class CarThread extends Thread {
-    String name;
-    int gateNO;
-    int arrivalTime;
-    long waitingTime = 0;
-    int parkingTime;
-    static Sema spots = new Sema(4);
+    private String name;
+    private int gateNO;
+    private int arrivalTime;
+    private long waitingTime = 0;
+    private int parkingTime;
+    private static Sema spots = new Sema(4);
 
+
+
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
 
     public CarThread(String name, int gateNO, int arrivalTime, int parkingTime) {
         this.name = name;
+        this.setName(name);
         this.gateNO = gateNO;
         this.arrivalTime = arrivalTime;
         this.parkingTime = parkingTime;
@@ -19,11 +24,11 @@ public class CarThread extends Thread {
 
 
     public void carArrival(){
-        System.out.println(name + " from Gate " + gateNO + " at time " + arrivalTime );
+        System.out.println(name + " from Gate " + gateNO + " arrived at time " + arrivalTime );
     }
 
     public void waitTime(){
-        System.out.println(name + "from Gate " + this.gateNO + " waiting for a spot");
+        System.out.println(name + " from Gate " + this.gateNO + " waiting for a spot");
         long startTime = System.currentTimeMillis();
         while (spots.availablePermits() == 0);
         long endTime = System.currentTimeMillis();
@@ -35,21 +40,21 @@ public class CarThread extends Thread {
         while (spots.isBusy()) {
             isWaiting = true;
             waitTime();
-            System.out.println(name + "from Gate " + this.gateNO + " parked after " + this.waitingTime + " milliseconds");
+            System.out.println(name + " from Gate " + this.gateNO + " parked after " + this.waitingTime + " milliseconds");
         }
 
         if(!isWaiting){
-            System.out.println(name + "from Gate " + this.gateNO + " parked");
+            System.out.print(name + " from Gate " + this.gateNO + " parked");
         }
+        System.out.println(" Parking Status: " + spots.availablePermits() + " spots occupied");
         spots.acquire();
-        System.out.println("Parking Status: " + spots.availablePermits() + " spots occupied");
         Thread.sleep(parkingTime);
     }
 
     public void carLeaving() throws InterruptedException {
+        System.out.print(name + " from Gate " + this.gateNO + " left after " + this.parkingTime + " milliseconds");
+        System.out.println(" Parking Status: " + spots.availablePermits() + " spots occupied");
         spots.release();
-        System.out.print(name + "from Gate " + this.gateNO + " left after " + this.parkingTime + " milliseconds");
-        System.out.println("Parking Status: " + spots.availablePermits() + " spots occupied");
     }
 
     public void run(){
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,46 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         File file = new File("input.txt");
-        Parser parser = new Parser();
-        parser.parse(file);
+        ArrayList<CarThread> carThreads = new ArrayList<>();
+
+        Scanner reader = new Scanner(file);
+        while(reader.hasNextLine()) {
+
+            String line = reader.nextLine().replaceAll(",", "");
+            String[] splitWords = line.split(" ");
+            String carName = splitWords[2] + " " + splitWords[3];
+            int gateNo = Integer.parseInt(splitWords[1]);
+            int arrivalTime = Integer.parseInt(splitWords[5]);
+            int parkingTime = Integer.parseInt(splitWords[7]);
+
+            CarThread carThread = new CarThread(carName, gateNo, arrivalTime, parkingTime);
+            carThreads.add(carThread);
+        }
+
+
+        carThreads.sort(Comparator.comparingInt(CarThread::getArrivalTime));
+//        for (CarThread carThread : carThreads) {
+//            System.out.println(carThread.getName() + " " +  carThread.getArrivalTime());
+//        }
+        for (CarThread carThread : carThreads) {
+            carThread.start();
+            Thread.sleep(1);
+        }
+
+        //carThreads.forEach(carThread -> carThread.start());
+
 
     }
 }
+
+
+
+
